bbd3a8c636dbcbefb3329d2a69b91e43d433ad79
Vincent LANDRA <gvince958@gmail.Com>
2015-10-20 11:45:57 +0200

    Mise a jour JDBC Final

--- bankonet-lib/src/com/bankonet/dao/client/ClientDaoSQL.java
+++ bankonet-lib/src/com/bankonet/dao/client/ClientDaoSQL.java
@@ -2,14 +2,17 @@ package com.bankonet.dao.client;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.bankonet.dao.dto.ClientComptesDTO;
 import com.bankonet.dao.dto.IdLibelleComptesDTO;
 import com.bankonet.utils.Client;
 import com.bankonet.utils.Compte;
+import com.bankonet.utils.others.Civilite;
 
 public class ClientDaoSQL implements ClientDao {
 
@@ -25,7 +28,6 @@ public class ClientDaoSQL implements ClientDao {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -41,11 +43,12 @@ public class ClientDaoSQL implements ClientDao {
 			statement.close();
 			for(Compte compte:client.getComptesList()) {
 				Statement st = bdd.createStatement();
-				
-				st.execute(
-						"INSERT INTO clients_comptes (login, intitule) VALUES ('"+client.getId()+"', '"+compte.getIntitule()+"')"+
-						"  ON DUPLICATE KEY UPDATE login='"+client.getId()+"',intitule='"+compte.getIntitule()+"';"
+				ResultSet resultat = st.executeQuery("SELECT * FROM clients_comptes WHERE login='"+client.getId()+"' && intitule='"+compte.getIntitule()+"';");
+				if(!resultat.next()){
+					st.execute(
+						"INSERT INTO clients_comptes (login, intitule) VALUES ('"+client.getId()+"', '"+compte.getIntitule()+"');"
 						);
+				}
 				
 				st.close();
 			}
@@ -57,32 +60,73 @@ public class ClientDaoSQL implements ClientDao {
 
 	@Override
 	public List<IdLibelleComptesDTO> retournerIdClients() {
+		List<IdLibelleComptesDTO> clientsDto = new ArrayList<IdLibelleComptesDTO>();
 		try(Connection bdd = DriverManager.getConnection(url, user, password);) {
+			Statement statement = bdd.createStatement();
+			ResultSet resultat = statement.executeQuery("SELECT login, nom, prenom FROM clients;");
+			while(resultat.next()){
 			
+				String login = resultat.getString("login");
+				String nom = resultat.getString("nom");
+				String prenom = resultat.getString("prenom");
+				String lib = "\nIdentifiant: "+login+" / Nom: "+nom+" / Prenom: "+prenom;
+				
+				Statement statement_comptes = bdd.createStatement();
+				ResultSet resultat_comptes = statement_comptes.executeQuery("SELECT intitule FROM clients_comptes WHERE login='"+login+"';");
+				ArrayList<String> comptesList = new ArrayList<String>();
+				while(resultat_comptes.next())
+					comptesList.add(resultat_comptes.getString("intitule"));
+				
+				clientsDto.add(new IdLibelleComptesDTO(login, lib, comptesList));
+			}
+			statement.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}	
-		return null;
+		return clientsDto;
 	}
 
 	@Override
 	public ClientComptesDTO chargerClient(String login) {
+		ClientComptesDTO clientDto = null;
 		try(Connection bdd = DriverManager.getConnection(url, user, password);) {
+			Statement statement = bdd.createStatement();
+			ResultSet resultat = statement.executeQuery("SELECT * FROM clients WHERE login='"+login+"';");
+			resultat.next();
 			
+			String mdp = resultat.getString("password");
+			Civilite civilite = Civilite.getCivilite(resultat.getString("civilite"));
+			String nom = resultat.getString("nom");
+			String prenom = resultat.getString("prenom");
+			
+			resultat = statement.executeQuery("SELECT intitule FROM clients_comptes WHERE login='"+login+"';");
+			ArrayList<String> comptesList = new ArrayList<String>();
+			while(resultat.next())
+				comptesList.add(resultat.getString("intitule"));
+			
+			clientDto = new ClientComptesDTO(new Client(login, mdp, civilite, nom, prenom), comptesList);
+			statement.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}	
-		return null;
+		return clientDto;
 	}
 
 	@Override
 	public boolean connexionClient(String login, String pmdp) {
+		boolean connexion = false;
 		try(Connection bdd = DriverManager.getConnection(url, user, password);) {
+			Statement statement = bdd.createStatement();
+			ResultSet resultat = statement.executeQuery("SELECT password FROM clients WHERE login='"+login+"';");
+			resultat.next();
+			if(pmdp.equals(resultat.getString("password")))
+				connexion = true;
 			
+			statement.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}	
-		return false;
+		return connexion;
 	}
 
 	@Override
--- bankonet-lib/src/com/bankonet/dao/compte/CompteDaoSQL.java
+++ bankonet-lib/src/com/bankonet/dao/compte/CompteDaoSQL.java
@@ -2,13 +2,16 @@ package com.bankonet.dao.compte;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.bankonet.utils.Compte;
 import com.bankonet.utils.CompteCourant;
 import com.bankonet.utils.CompteEpargne;
+import com.bankonet.utils.others.TypeCompte;
 
 public class CompteDaoSQL implements CompteDao {
 
@@ -30,12 +33,26 @@ public class CompteDaoSQL implements CompteDao {
 	
 	@Override
 	public List<Compte> chargerComptes(List<String> comptesString) {
+		List<Compte> comptes = new ArrayList<Compte>();
 		try(Connection bdd = DriverManager.getConnection(url, user, password);) {
-			
+			for(String intitule:comptesString){
+				Statement statement = bdd.createStatement();
+				ResultSet resultat = statement.executeQuery("SELECT * FROM comptes WHERE intitule='"+intitule+"';");
+				resultat.next();
+				
+				String numero = resultat.getString("numero");
+				TypeCompte type = TypeCompte.getType(resultat.getString("type"));
+				String libelle = resultat.getString("libelle");
+				double solde = resultat.getDouble("solde");
+				
+				if(type.getValue().equals("Courant")) comptes.add(new CompteCourant(numero, type, intitule, libelle, solde, resultat.getFloat("decouvert")));
+				else comptes.add(new CompteEpargne(numero, type, intitule, libelle, solde, resultat.getFloat("taux")));
+				statement.close();
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}	
-		return null;
+		return comptes;
 	}
 
 	@Override
@@ -54,8 +71,8 @@ public class CompteDaoSQL implements CompteDao {
 					taux = (float)((CompteEpargne)compte).getTauxInteret();
 				}
 				statement.execute(
-						"INSERT INTO comptes (intitule, type, libelle, solde, decouvert, taux) VALUES ('"+compte.getIntitule()+"', '"+compte.getType()+"', '"+compte.getLibelle()+"', '"+compte.getSolde()+"', "+decouvert+", "+taux+")"+
-						"  ON DUPLICATE KEY UPDATE intitule='"+compte.getIntitule()+"', type='"+compte.getType()+"', libelle='"+compte.getLibelle()+"', solde='"+compte.getSolde()+"', decouvert="+decouvert+", taux="+taux+" ;"
+						"INSERT INTO comptes (intitule, numero, type, libelle, solde, decouvert, taux) VALUES ('"+compte.getIntitule()+"', '"+compte.getNumero()+"', '"+compte.getType()+"', '"+compte.getLibelle()+"', '"+compte.getSolde()+"', "+decouvert+", "+taux+")"+
+						"  ON DUPLICATE KEY UPDATE intitule='"+compte.getIntitule()+"', numero='"+compte.getNumero()+"', type='"+compte.getType()+"', libelle='"+compte.getLibelle()+"', solde='"+compte.getSolde()+"', decouvert="+decouvert+", taux="+taux+" ;"
 						);
 				statement.close();
 			}
